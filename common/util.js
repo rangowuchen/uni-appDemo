74da7d13b6737d3ea499b28ec1068ab5f723a497
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2019-01-15 16:53:23 +0800

    首页列表与详情

---
+++ common/util.js
@@ -0,0 +1,105 @@
+var list = [{
+		author_name: "张雨忻",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14231055/qs97rbe4xzyfke7o!heading",
+		id: 80164,
+		post_id: "5172213",
+		published_at: "2019-01-15 07:29:02",
+		title: "8点1氪 | 字节跳动将推出的社交产品疑为“多闪”，DST旗下基金减持小米，美团称招聘游戏人才就是“试试”",
+		img_num:"10"
+	},
+	{
+		author_name: "Tech星球",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14155237/5oovd3jtpvt83hfq!heading",
+		id: 80165,
+		post_id: "5172210",
+		published_at: "2019-01-15 07:27:03",
+		title: "揭秘王欣、张一鸣、罗永浩将发布的3款社交产品,「马桶」还未上线已被微信封杀",
+		img_num:"4"
+	}, {
+		author_name: "CSDN",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14232344/tcjo12jmb1ehdh12!heading",
+		id: 80166,
+		post_id: "5172220",
+		published_at: "2019-01-15 07:24:43",
+		title: "Google正在“跟踪”你",
+		img_num:"9"
+	}, {
+		author_name: "宋长乐",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14180856/4pe55puxsfdnt4n8!heading",
+		id: 80160,
+		post_id: "5172214",
+		published_at: "2019-01-15 07:21:05",
+		title: "把果粉锁进自家“小黑屋”的HomePod，更像是智能家居场景的AirPods",
+		img_num:"10"
+	}, {
+		author_name: "零售老板内参",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14231822/jrkl95dlkygkkwsz!heading",
+		id: 80161,
+		post_id: "5172218",
+		published_at: "2019-01-15 07:19:41",
+		title: "传瑞幸最快上半年赴港IPO，巨亏也要上？",
+		img_num:"10"
+	}, {
+		author_name: "懂懂笔记",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14123454/ph6ifzjjy1qi6e0c!heading",
+		id: 80162,
+		post_id: "5172187",
+		published_at: "2019-01-15 07:10:05",
+		title: "苹果“崴脚”，供应商“骨折”",
+		img_num:"8"
+	}, {
+		author_name: "脑极体",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14110121/to14c71r12b4dyu3!heading",
+		id: 80159,
+		post_id: "5172178",
+		published_at: "2019-01-15 07:00:05",
+		title: "我们终将泄露的人脸数据，后果到底有多可怕？",
+		img_num:"6"
+	}, {
+		author_name: "全媒派",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14144046/77nrptue5jr3vw7a!heading",
+		id: 80158,
+		post_id: "5172204",
+		published_at: "2019-01-14 22:40:53",
+		title: "「纳米网红」帝国的崛起：小人国里风生水起的流量&口碑生意",
+		img_num:"5"
+	}, {
+		author_name: "IT桔子",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14143317/n2dojvx1koy7ii56!heading",
+		id: 80157,
+		post_id: "5172203",
+		published_at: "2019-01-14 22:33:26",
+		title: "深度解读腾讯2018年投资布局：平均每月投资13.6家公司，比去年节奏更紧凑",
+		img_num:"10"
+	}, {
+		author_name: "Yunnn",
+		comments_count: 0,
+		content: "",
+		cover: "https://pic.36krcnd.com/201901/14140930/xmn0ydbu1fw35cqf!heading",
+		id: 80156,
+		post_id: "5172196",
+		published_at: "2019-01-14 22:11:24",
+		title: "专注肿瘤基因测序领域，「吉因加」吉因加获得华大基因5000万融资",
+		img_num:"4"
+	}
+]
+
+export default {list}
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,39 +1,74 @@
 <template>
-	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
-		<view>
-            <text class="title">{{title}}</text>
-        </view>
+	<view class="index">
+		<block v-for="(item, index) in list" :key="index">
+			<view class="card" @click="goDetail(item)">
+				<image class="card-img" :src="item.cover" mode="aspectFill"></image>
+				<text class="card-num-view">{{item.img_num}}P</text>
+				<view class="card-bottm row">
+					<view class="car-title-view row">
+						<text class="card-title">{{item.title}}</text>
+					</view>
+					<view @click.stop="share(item)" class="card-share-view">{{item.author_name}}</view>
+				</view>
+			</view>
+		</block>
+		<view class="loadMore" v-show="fetchPageNum!=3">加载中...</view>
 	</view>
 </template>
 
 <script>
+	import aa from '../../common/util.js'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				list: [],
+				fetchPageNum: 1
 			}
 		},
 		onLoad() {
-
+			this.getData();
+			console.log(aa.list)
+		},
+		onReachBottom() {
+			console.log("滑动到页面底部")
+			this.getData();
+		},
+		onPullDownRefresh() {
+			console.log("下拉刷新");
+			this.getData();
 		},
 		methods: {
+			getData() {
+				if (this.fetchPageNum == 1) {
+					uni.stopPullDownRefresh()
+					this.list = aa.list;
+					this.fetchPageNum = 2;
+					return;
+				}
+				if (this.fetchPageNum == 2) {
+					this.list = this.list.concat(this.list);
+					this.fetchPageNum += 1;
+					return;
+				}
+				if (this.fetchPageNum == 3) {
+					uni.showToast({
+						title: "已经最新",
+						icon: "none",
+					})
+					uni.stopPullDownRefresh();
+					return;
+				}
+			},
+			goDetail(e) {
+				console.log(e)
+				uni.navigateTo({
+					url: "../index-detail/index-detail"
+				})
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		text-align: center;
-		height: 400upx;
-	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
-	}
+
 </style>
